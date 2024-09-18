325e5b6a1b88b677f0378b1686762b734125aa36
Wojtek <returnpll@gmail.com>
2024-09-18 23:50:15 +0200

    porownywanie ukladow. test

--- backend/deck.py
+++ backend/deck.py
@@ -1,3 +1,4 @@
+import random
 from card import Card
 
 class Deck:
@@ -10,4 +11,12 @@ class Deck:
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         return [Card(suit, rank) for suit in suits for rank in ranks]
 
+    def generate_random_cards(self, n):
+        if n > 52:   # Wiadomo. 52 kart w talii
+            raise ValueError("You can't draw more cards than there are in the deck (52)")
+        return random.sample(self.deck, n)
 
+#deck = Deck()
+#random_cards = deck.generate_random_cards(53)
+#for card in random_cards:
+#    print(card)
