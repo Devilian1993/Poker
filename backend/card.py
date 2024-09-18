0636d5e989abc1b55b06d6047240e72b7c81e2ae
Devilian1993 <Maksioczko@gmail.com>
2024-09-18 12:56:52 +0200

    Funkcja next_card do klasy card
    Funkcja zwraca następną w kolejności kartę dla każdych kart poza asem. Dla asa zwraca None

--- backend/card.py
+++ backend/card.py
@@ -6,3 +6,19 @@ class Card:
 
     def __repr__(self):
         return f"{self.value} of {self.suit}"
+
+    def next_card(self):
+        if isinstance(self.value, int):
+            if self.value == 10:
+                return "J"
+            else:
+                return self.value + 1
+        else:
+            if self.value == "J":
+                return "Q"
+            elif self.value == "Q":
+                return "K"
+            elif self.value == "K":
+                return "A"
+            else:
+                return None
