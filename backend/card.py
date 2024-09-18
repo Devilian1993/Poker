b79fc68bb9c9b733a90dc5a8e4a9cbfa1ac34269
Devilian1993 <Maksioczko@gmail.com>
2024-09-18 15:37:20 +0200

    Porównywanie kart
    Dodano metody __lt__, __eq__ i __gt__ do klasy Card, żeby dało się je łatwo porównywać bez używania dodatkowych funkcji

--- backend/card.py
+++ backend/card.py
@@ -22,3 +22,18 @@ class Card:
                 return "A"
             else:
                 return 2
+
+    def __lt__(self, other):
+        util_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+             'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+        return util_values[self.value] < util_values[other.value]
+
+    def __eq__(self, other):
+        util_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+                       'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+        return util_values[self.value] == util_values[other.value]
+
+    def __gt__(self, other):
+        util_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+                       'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+        return util_values[self.value] > util_values[other.value]
