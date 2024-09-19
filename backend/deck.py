ddeafc404f0c4d1ed5756c5b3460838e85e5beab
Devilian1993 <Maksioczko@gmail.com>
2024-09-19 12:32:54 +0200

    Deck bug naprawiony
    Teraz są właściwe typy danych (int dla liczb str dla figur)

--- backend/deck.py
+++ backend/deck.py
@@ -11,7 +11,7 @@ class Deck:
 
     def generate_full_deck(self):
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
         return [Card(suit, rank) for suit in suits for rank in ranks]
 
     def generate_random_cards(self, n):
